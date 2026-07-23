52e3c9d6d1dfcb5c4371e5af095f235a8689dbd2
Matt <myatthuta1835@gmail.com>
2026-07-23 12:36:03 +0630

    Edit config.js of both admin and blog to make them environment aware

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,1 +1,3 @@
-export const API_BASE = "http://localhost:3000/api";
+export const API_BASE = window.location.hostname === "localhost" 
+    ? "http://localhost:3000/api"
+    : "https://splash-api-h077.onrender.com/api";
